7ef9265ae9282b817700b74b17c720a288e88e2e
Arnon Portal <arnon.portal@gmail.com>
2021-12-13 15:59:55 +0200

    Popup Change

--- getting-started/popup.js
+++ getting-started/popup.js
@@ -1,6 +1,7 @@
 // Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
-
+// let encrypted = true;
+chrome.storage.sync.set({encrypted : true});
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
@@ -15,10 +16,40 @@ changeColor.addEventListener("click", async () => {
   });
 });
 
+
+// function decrypt(str) {
+//   let newStr = "";
+//   for (let i = 0; i < str.length; i++) {
+//     newStr = newStr + (str.charAt(i) + 2);
+//   }
+//   return newStr;
+// }
+
+
 // The body of this function will be execuetd as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+  let table = document.getElementById("dataTable");
+  let newTable = table;
+  chrome.storage.sync.get(['encrypted'],({encrypted}) =>{
+    
+    for (let i = 1; i < table.rows.length; i++) {
+      for (let j = 0; j < table.rows[i].length; j++) {
+      newTable.rows[i].cells[j].innerHTML = decryptEncrypt(table.rows[i].cells[j].innerHTML,encrypted);
+      }
+    }
+    
+    // chrome.storage.sync.set({encrypted: !encrypted});
   });
+  console.log(newTable);
+  table.replaceWith(newTable);
+}
+
+
+function decryptEncrypt(str,encrypted) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    newStr += encrypted ? (str.charAt(i) + 2) : ((str.charAt(i) - 2));
+  }
+  return newStr;
 }
