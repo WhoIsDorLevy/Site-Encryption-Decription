597c798d4a79bb6b74afd8f47de08d825a55fced
Dor Levy <lwydwr@gmail.com>
2021-12-14 22:48:51 +0200

    submit button and design changes

--- getting-started/popup.js
+++ getting-started/popup.js
@@ -1,10 +1,29 @@
-// Initialize button
+// Initialize buttons
 let encryptButton = document.getElementById("encrypt");
+let submitButton = document.getElementById("submitButton");
 
-//Add global variable to storage
+//Add global variables to storage
 chrome.storage.sync.set({ encrypted: true });
+chrome.storage.sync.set({ encryptionKey: 0 });
 
-// When the button is clicked, inject encryption into current page
+//add on click listeners to the buttons
+
+//when the "submit key" button is clicked, set the encryption key to the input
+submitButton.addEventListener("click", async () => {
+  let textBox = document.getElementById("keyInput");
+  let keyInput = textBox.value;
+  let keyInteger = parseInt(keyInput);
+  textBox.value = '';
+  if (isNaN(keyInteger)){
+    alert("Please provide a valid number")
+  }
+  else {
+    chrome.storage.sync.set({ encryptionKey: keyInteger })
+    alert("Key submitted successfully")
+  }
+} )
+
+// When the encryption button is clicked, inject encryption key into current page
 encryptButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
@@ -20,30 +39,31 @@ encryptButton.addEventListener("click", async () => {
 function encrypt() {
   let currTable = document.getElementById("dataTable");
   if (currTable == null) {
-    alert("This extension is for Dor&Po agency use only !");
+    alert("This extension is for Dor&Po agency use only!");
   } 
   else {
   let newTable = currTable;
-  let decryptEncrypt = (str, encrypted) => {
+  let decryptEncrypt = (str, encrypted, encryptionKey) => {
     let newStr = "";
     for (let i = 0; i < str.length; i++) {
       newChar = encrypted
-        ? String.fromCharCode(str.charCodeAt(i) + 2)
-        : String.fromCharCode(str.charCodeAt(i) - 2);
+        ? String.fromCharCode(str.charCodeAt(i) + encryptionKey)
+        : String.fromCharCode(str.charCodeAt(i) - encryptionKey);
       newStr += newChar;
       }
     return newStr;
   } 
-  chrome.storage.sync.get(["encrypted"], ({ encrypted }) => {
+  chrome.storage.sync.get(["encrypted", "encryptionKey"], ( res ) => {
     for (let i = 1; i < newTable.rows.length; i++) {
       for (let j = 0; j < newTable.rows[i].cells.length; j++) {
         newTable.rows[i].cells[j].innerHTML = decryptEncrypt(
           newTable.rows[i].cells[j].innerHTML,
-          encrypted
+          res.encrypted,
+          res.encryptionKey
         );
       }
     }
-  chrome.storage.sync.set({ encrypted: !encrypted });
+  chrome.storage.sync.set({ encrypted: !res.encrypted });
   });
   currTable.replaceWith(newTable);
   }
